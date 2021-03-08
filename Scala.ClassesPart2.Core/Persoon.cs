ddf94ada17c89e3643fdb3d7d3f562bc874cf228
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-03-08 09:13:49 +0100

    Add project files.

---
+++ Scala.ClassesPart2.Core/Persoon.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scala.ClassesPart2.Core
+{
+    public class Persoon
+    {
+        private string naam;
+        private string voornaam;
+        private DateTime geboortedatum;
+
+        public string Naam
+        {
+            get { return naam; }
+            set
+            {
+                value = value.Trim();
+                if (value.Length == 0)
+                {
+                    throw new Exception("De naam is verplicht !");
+                }
+                else
+                {
+                    naam = value;
+                }
+            }
+        }
+        public string Voornaam
+        {
+            get { return voornaam; }
+            set
+            {
+                value = value.Trim();
+                if (value.Length == 0)
+                {
+                    throw new Exception("De voornaam is verplicht !");
+                }
+                else
+                {
+                    voornaam = value;
+                }
+            }
+        }
+
+        public DateTime Geboortedatum
+        {
+            get { return geboortedatum; }
+            set
+            {
+                DateTime minimumDatum = new DateTime(1900, 1, 1);
+                if (value < minimumDatum)
+                {
+                    throw new Exception("Oudst mogelijke datum is 1/1/1900 !");
+                }
+                else if (value > DateTime.Now)
+                {
+                    throw new Exception("Geboortedatum kan niet in toekomst liggen !");
+                }
+                else
+                {
+                    geboortedatum = value;
+                }
+            }
+        }
+        public bool IsMan { get; set; }
+
+        public string Leeftijd
+        {
+            get
+            {
+                DateTime vandaag = DateTime.Now;
+                TimeSpan verschil = vandaag - Geboortedatum;
+                DateTime leeftijd = DateTime.MinValue + verschil;
+                return $"{leeftijd.Year-1} jaar, {leeftijd.Month-1} maanden en {leeftijd.Day-1} dagen";
+            }
+        }
+        
+
+        public Persoon()
+        {
+
+        }
+        public Persoon(string naam, string voornaam, DateTime geboortedatum, bool isman)
+        {
+            Naam = naam;
+            Voornaam = voornaam;
+            Geboortedatum = geboortedatum;
+            IsMan = isman;
+        }
+
+        public override string ToString()
+        {
+            return $"{Naam} {Voornaam}";
+        }
+    }
+}
---
+++ Scala.ClassesPart2.Core/PersoonService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Scala.ClassesPart2.Core
+{
+    public class PersoonService
+    {
+        public List<Persoon> Personen { get; private set; }
+        public PersoonService()
+        {
+            Personen = new List<Persoon>();
+            DoSeeding();
+
+        }
+        private void DoSeeding()
+        {
+            try
+            {
+                Personen.Add(new Persoon("Janssens", "Wim", new DateTime(1995, 12, 24), true));
+                Personen.Add(new Persoon("Willems", "Jan", new DateTime(1985, 5, 1), true));
+                Personen.Add(new Persoon("Pieters", "Mia", new DateTime(1999, 12, 3), false));
+                Personen.Add(new Persoon("Abeels", "Wilma", new DateTime(2003, 7, 23), false));
+                Personen.Add(new Persoon("Taer", "Guy", new DateTime(1990, 1, 1), true));
+            }
+            catch
+            { }
+        }
+        public void OrderList()
+        {
+            Personen = Personen.OrderBy(o => o.Naam).ThenBy(o => o.Voornaam).ToList();
+        }
+        public void VoegPersoonToe(Persoon persoon)
+        {
+            Personen.Add(persoon);
+            OrderList();
+        }
+        public void VerwijderPersoon(Persoon persoon)
+        {
+            Personen.Remove(persoon);
+            OrderList();
+        }
+    }
+}
